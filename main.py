d537a2116d4d8bf31d3a743ebe90dadd9b77c73c
pallavikandibilla <71749075+pallavikandibilla@users.noreply.github.com>
2024-03-27 23:01:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+import pyttsx3
+import PyPDF2
+
+book = open('punk57.pdf','rb')
+pdf_reader = PyPDF2.PdfFileReader(book)
+num_pages = pdf_reader.numPages
+
+play = pyttsx3.init()
+print('Playing Audio Book')
+
+for num in range(0, num_pages):
+	page = pdf_reader.getPage(num)
+	data = page.extractText()
+
+	play.say(data)
+	play.runAndWait()
